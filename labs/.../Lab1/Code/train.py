9d28ae0b210720c9b2703be045cbd85c1cd82aff
Andrea F. Daniele <afdaniele@ttic.edu>
2018-01-23 14:41:28 -0600

    some code for parsing data in Lab1 and template for project proposal

---
+++ labs/.../Lab1/Code/train.py
@@ -0,0 +1,14 @@
+# @Author: Andrea F. Daniele <afdaniele>
+# @Date:   Monday, January 22nd 2018
+# @Email:  afdaniele@ttic.edu
+# @Last modified by:   afdaniele
+# @Last modified time: Monday, January 22nd 2018
+
+
+from utils import load_data
+import numpy as np
+
+# get data
+datasets = load_data( data_dir="../Data", verbose=True )
+
+# compute features
---
+++ labs/.../Lab1/Code/utils/__init__.py
@@ -0,0 +1,9 @@
+# @Author: Andrea F. Daniele <afdaniele>
+# @Date:   Monday, January 22nd 2018
+# @Email:  afdaniele@ttic.edu
+# @Last modified by:   afdaniele
+# @Last modified time: Monday, January 22nd 2018
+
+
+from .progress_bar import *
+from .parse_data import *
