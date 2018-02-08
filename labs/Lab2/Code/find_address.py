ff3330555b07855711269d409f2a7099a966d9ca
Noah Hirsch <noahhirsch@flash.uchicago.edu>
2018-02-08 15:31:07 -0600

    adding code

---
+++ labs/Lab2/Code/find_address.py
@@ -0,0 +1,12 @@
+import numpy as np
+import json
+import localize
+import sys
+
+print("Mobile Computing | Lab 2 | Localization")
+print("Andrea F. Daniele, Max X. Liu, Noah A. Hirsch")
+print()
+print("Usage: python3.6 find_address.py [mac-address]")
+
+
+address = sys.argv[1]
