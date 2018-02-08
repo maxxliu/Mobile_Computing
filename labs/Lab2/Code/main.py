1416741dfdca0221a4249732eddbc489a066b533
Andrea F. Daniele <afdaniele@ttic.edu>
2018-02-08 16:08:12 -0600

    data loader completed

---
+++ labs/Lab2/Code/main.py
@@ -0,0 +1,10 @@
+# @Author: Andrea F. Daniele <afdaniele>
+# @Date:   Thursday, February 8th 2018
+# @Email:  afdaniele@ttic.edu
+# @Last modified by:   afdaniele
+# @Last modified time: Thursday, February 8th 2018
+
+
+from utils import *
+
+ID_to_MAC, MAC_to_ID, data = load_data('../Data/rssdataset')
