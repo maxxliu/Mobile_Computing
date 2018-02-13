4f6198b2d5287985a9d507b312e2967cf0f8256e
maxxliu <maxxliu19@gmail.com>
2018-02-13 10:08:06 -0600

    included best fit curve

--- labs/Lab2/Code/distance_relationship.py
+++ labs/Lab2/Code/distance_relationship.py
@@ -33,8 +33,8 @@ def get_distance():
                 dist = math.hypot(d['loc_x'] - DEVICE_DATA[mac]['x'],
                                         d['loc_y'] - DEVICE_DATA[mac]['y'])
 
-            distance.append(dist)
-            rss.append(int(d['rss']))
+                distance.append(dist)
+                rss.append(int(d['rss']))
 
     distance = np.array(distance)
     rss = np.array(rss) * -1
@@ -54,7 +54,7 @@ def bucket_rss(distance, rss):
         RSS_buckets.append([])
 
     for i, val in enumerate(rss):
-        RSS_buckets[val].append(distance[i])
+        RSS_buckets[int(val)].append(distance[i])
 
     return RSS_buckets
 
@@ -68,6 +68,7 @@ def calculate_buckets(RSS_buckets, b_size):
     rss_values = []
     for i, val in enumerate(RSS_buckets):
         if len(val) > 0:
+            # print "rss value: %i number of observations: %i" % (i, len(val))
             rss_values.append(i)
 
     # find the most common distance for each rss value
@@ -86,11 +87,10 @@ def calculate_buckets(RSS_buckets, b_size):
 def run_simple_distance_relationship():
     '''
     just goes through each rss and finds most common bucket for distance
-    gives pretty bad results
     '''
     distance, rss = get_distance()
     RSS_buckets = bucket_rss(distance, rss)
-    rss_values, dist = calculate_buckets(RSS_buckets, 50)
+    rss_values, dist = calculate_buckets(RSS_buckets, 20)
 
     # plot rss vs distance
     plt.close()
@@ -100,10 +100,77 @@ def run_simple_distance_relationship():
     plt.show()
 
 
-def sliding_window():
+def moving_average(a, size):
     '''
-    looks at a frame of data and throws away outliers
+    a - np array
+    size - size of window
     '''
+    ret = np.cumsum(a)
+    ret[size:] = ret[size:] - ret[:-size]
+    return ret[size - 1:] / size
+
+
+def load_data_ma(wsize=4):
+    '''
+    loads data but takes moving average first
+    '''
+    # Loop through each of the files and calucalate distance for the
+    # known devices
+    distance = np.array([])
+    rss = np.array([])
+
+    for f in FILES:
+        with open(f, 'r') as data_file:
+            json_data = data_file.read()
+        data = json.loads(json_data)
+
+        dist_temp = []
+        rss_temp = []
+        for d in data:
+            mac = d['mac']
+            if mac in DEVICE_DATA:
+                dist = math.hypot(d['loc_x'] - DEVICE_DATA[mac]['x'],
+                                        d['loc_y'] - DEVICE_DATA[mac]['y'])
+
+                dist_temp.append(dist)
+                rss_temp.append(int(d['rss']))
+
+        dist_temp = moving_average(np.array(dist_temp), wsize)
+        rss_temp = np.array(rss_temp) * -1
+        rss_temp = moving_average(np.array(rss_temp), wsize)
+
+        distance = np.hstack([distance, dist_temp])
+        rss = np.hstack([rss, rss_temp])
+
+    return distance, rss
+
+
+def run_moving_avg_distance_relationship(bsize=20, wsize=4):
+    '''
+    create relationship using moving average
+    '''
+    distance, rss = load_data_ma(wsize)
+    RSS_buckets = bucket_rss(distance, rss)
+    rss_values, dist = calculate_buckets(RSS_buckets, bsize)
+
+    # only want to use the range from [39, 64]
+    rss_values = np.array(rss_values[:-6])
+    dist = np.array(dist[:-6])
+
+    # return rss_values, dist
+
+    # find best fit line
+    z = np.polyfit(rss_values, dist, 2)
+    p2 = np.poly1d(z)
+
+    xp = np.linspace(30, 70, 100)
+
+    # plot rss vs distance
+    plt.close()
+    plt.plot(rss_values, dist, xp, p2(xp), '-')
+    plt.xlabel('RSS')
+    plt.ylabel('Distance')
+    plt.show()
 
 
 # # Create heatmap
