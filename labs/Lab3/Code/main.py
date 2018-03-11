fcb2773c9d29ac60d0a8ad11b66a3e674714e2cc
Andrea F. Daniele <afdaniele@ttic.edu>
2018-03-10 22:47:12 -0600

    code cleaned and submission milestone for Lab#3

--- labs/Lab3/Code/main.py
+++ labs/Lab3/Code/main.py
@@ -6,68 +6,76 @@ from detector_wrapper import DetectorWrapper
 import time
 import numpy as np
 
+# create line detector
 detector = DetectorWrapper()
 
+# initialize interface to front and back wheels
 front_wheels = Front_Wheels()
 back_wheels = Back_Wheels()
 
-max_turn = 28
+# set parameters
 max_speed = 20
 turn_gain = .25
 turn_offset = -2
 x_offset = 0
-
 use_memory = False
 memory = []
 memory_size = 3
 
+# stop the car and set the front wheels straight
 back_wheels.stop()
 front_wheels.turn_rel( turn_offset )
 
+# the adventure begins
 i = 0
 try:
     while True:
+        # get new frame from the camera and detect lines
         success, ret = detector.detect()
         detector.plot(ret)
 
+        # if the lines were successfully detected
         if success:
             i += 1
 
+            # set speed and direction for back_wheels
             back_wheels.speed = max_speed
             back_wheels.forward()
 
+            # get info about the lines
             frame, mid_x, left_fit, right_fit, ploty, left_fitx, right_fitx = ret
 
-
-            # mid_x = float(sum(memory)) / float(len(memory))
-
+            # get ideal position for mid point (we found 300 to work better than img_width/2)
             img_width = frame.shape[0]
+            ideal_mid_x = 300
 
-            ideal_mid_x = 300 #float(img_width) / 2.0 + x_offset
-
+            # compute error
             error = mid_x - ideal_mid_x
 
+            # get a turning angle that is proportional to the error
             turn_angle = turn_offset + error * turn_gain
 
+            # skip the first 8 frames from the camera due to lighting artifact that allucinate phantom lines
             if i < 8: continue
 
-            # memory = memory[-memory_size:]
+            # store action in memory
             memory.append( turn_angle )
 
+            # compute average angle (in case of memory-based auto-pilot)
             if use_memory:
                 turn_angle = 0.0
                 mem_size = min(len(memory),memory_size)
                 for i in range(1,mem_size,1):
                     turn_angle += ( float(mem_size-i+1)/float(mem_size) ) * memory[-i]
 
+            # some debugging info
             print 'mid-point @ %d pxs / %d pxs, turning @ %.2f' % (mid_x, img_width, turn_angle)
 
+            # turn to correct the heading error
             front_wheels.turn_rel( turn_angle )
-            # time.sleep(0.5)
-            # front_wheels.turn_rel( turn_offset )
         else:
+            # no lines found, STOP
             back_wheels.stop()
-
 
 except KeyboardInterrupt:
     print("KeboardInterrupt Captured")
